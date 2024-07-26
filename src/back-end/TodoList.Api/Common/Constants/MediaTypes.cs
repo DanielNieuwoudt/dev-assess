40601cdc7ba0fb1ad8ff8dadbcebc93fbbd2cf29
Daniel Nieuwoudt <daniel@xerabeth.com>
2024-07-27 07:05:14 +1200

    Add ExcludeFromCodeCoverage attribute

--- src/back-end/TodoList.Api/Common/Constants/MediaTypes.cs
+++ src/back-end/TodoList.Api/Common/Constants/MediaTypes.cs
@@ -1,5 +1,8 @@
-﻿namespace TodoList.Api.Common.Constants
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TodoList.Api.Common.Constants
 {
+    [ExcludeFromCodeCoverage(Justification = "Constant by definition")]
     public class MediaTypes
     {
         public const string ApplicationProblemJson = "application/problem+json";
